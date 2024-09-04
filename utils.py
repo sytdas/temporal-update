e09b9edaf0770f646c55b0cd49602856ff836608
sytdas <sytd@athena.studio>
2024-09-04 11:45:42 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,3 @@
-import os
-
-import yaml
-from temporalio.client import TLSConfig
-
-
 def get_temporal_connection(env: str):
     address, tls, namespace = "localhost:7233", False, "default"
     return address, tls, namespace
